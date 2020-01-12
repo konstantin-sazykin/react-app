e77308e8c57f09181554e0eb4f6ba081ddec9cb2
Konstantin <konstantin-sazykin@github.com>
2020-01-12 16:14:58 +0400

    Updates

---
+++ precache-manifest.9ade849502951de042437f81c4798974.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f48a1a3362c7bd82a26e5a38a63ecef3",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "527c7f661bee84733517",
+    "url": "/react-app/static/css/main.03a8a7c8.chunk.css"
+  },
+  {
+    "revision": "2d2b0f1dec5506c2b109",
+    "url": "/react-app/static/js/2.c4148d08.chunk.js"
+  },
+  {
+    "revision": "527c7f661bee84733517",
+    "url": "/react-app/static/js/main.c4277814.chunk.js"
+  },
+  {
+    "revision": "c806dd8b4c6f8d060d44",
+    "url": "/react-app/static/js/runtime-main.dc2d0fdb.js"
+  },
+  {
+    "revision": "a68cb55946c8bb06186d649aa7c8ba77",
+    "url": "/react-app/static/media/preloader.a68cb559.svg"
+  },
+  {
+    "revision": "5648f452646ada8bc713327bddf1f86b",
+    "url": "/react-app/static/media/user.5648f452.png"
+  }
+]);
